210019608e5cfb65f015a3d3826d0edf68b4cdae
tylim <limteikyee@gmail.com>
2024-01-29 12:29:52 +0800

    use slider for attribute

--- src/component/Attributes.tsx
+++ src/component/Attributes.tsx
@@ -1,10 +1,7 @@
-import { Grid, Text, Stack, Flex, ActionIcon, TextProps } from '@mantine/core'
+import { Grid, Text, Stack, TextProps, Slider } from '@mantine/core'
 import { Characters } from '@/validation'
 import { useCharactersStore } from '@/stores'
-import {
-	ArrowForwardIosOutlined,
-	ArrowBackIosOutlined,
-} from '@mui/icons-material'
+import { theme } from '@/theme'
 
 const Component = ({
 	label,
@@ -71,79 +68,76 @@ export const Attributes = ({ character }: { character: Characters }) => {
 		character.attributes.atk.min
 
 	return (
-		<Stack align="center" gap={0} pt="xl">
-			<Flex w="100%">
-				<Flex justify="center" style={{ flexGrow: 1 }}>
-					<ActionIcon
-						variant="transparent"
-						style={{ flexGrow: 1 }}
-						onClick={() => {
-							if (level > 1) {
-								useCharactersStore.setState(state => {
-									state.slider.attributes[character.name] =
-										(state.slider.attributes[character.name] || 0) - 1
-								})
-							}
-						}}
-					>
-						<ArrowBackIosOutlined fontSize="large" htmlColor="black" />
-					</ActionIcon>
-				</Flex>
-				<Stack style={{ flexGrow: 1 }}>
-					<Component
-						label="Level"
-						value={`${level}`}
-						textProps={{ size: 'xl', fw: 'bold' }}
-					/>
-					<Component
-						label="HP"
-						value={`${Math.ceil(totalHP)}`}
-						textProps={{ fw: 'bold' }}
-					/>
-					<Component
-						label={character.dmg_type === 'magic' ? 'Magic ATK' : 'ATK'}
-						value={`${Math.ceil(totalATK)}`}
-						textProps={{ fw: 'bold' }}
-					/>
-					<Component
-						label={'Crit Rate'}
-						value={`${character.attributes.crit_rate}%`}
-					/>
-					<Component
-						label={'Crit DMG'}
-						value={`${character.attributes.crit_dmg}%`}
-					/>
-					<Component label={'DEF'} value={`${character.attributes.def}%`} />
-					<Component
-						label={'Magic Resist'}
-						value={`${character.attributes.magic_resist}%`}
-					/>
-					<Component
-						label={`${character.element === 'dark' ? 'Darkness' : character.element} DMG`}
-						value={`${character.attributes.dmg}%`}
-					/>
-					<Component
-						label={`${character.element === 'dark' || character.element === 'light' ? 'Property' : resistance[character.element]} Resist`}
-						value={`${character.attributes.dmg}%`}
-					/>
-				</Stack>
-				<Flex
-					justify="center"
-					style={{ flexGrow: 1 }}
-					onClick={() => {
-						if (level < 100) {
-							useCharactersStore.setState(state => {
-								state.slider.attributes[character.name] =
-									(state.slider.attributes[character.name] || 0) + 1
-							})
+		<Stack align="center" py="xl">
+			<Component
+				label="Level"
+				value={`${level}`}
+				textProps={{ size: 'xl', fw: 'bold' }}
+			/>
+			<Component
+				label="HP"
+				value={`${Math.ceil(totalHP)}`}
+				textProps={{ fw: 'bold' }}
+			/>
+			<Component
+				label={character.dmg_type === 'magic' ? 'Magic ATK' : 'ATK'}
+				value={`${Math.ceil(totalATK)}`}
+				textProps={{ fw: 'bold' }}
+			/>
+			<Component
+				label={'Crit Rate'}
+				value={`${character.attributes.crit_rate}%`}
+			/>
+			<Component
+				label={'Crit DMG'}
+				value={`${character.attributes.crit_dmg}%`}
+			/>
+			<Component label={'DEF'} value={`${character.attributes.def}%`} />
+			<Component
+				label={'Magic Resist'}
+				value={`${character.attributes.magic_resist}%`}
+			/>
+			<Component
+				label={`${character.element === 'dark' ? 'Darkness' : character.element} DMG`}
+				value={`${character.attributes.dmg}%`}
+			/>
+			<Component
+				label={`${character.element === 'dark' || character.element === 'light' ? 'Property' : resistance[character.element]} Resist`}
+				value={`${character.attributes.dmg}%`}
+			/>
+
+			<Slider
+				value={level}
+				onChange={value => {
+					useCharactersStore.setState(state => {
+						state.slider.attributes[character.name] = value
+					})
+				}}
+				color="blue"
+				min={1}
+				max={100}
+				step={1}
+				w="100%"
+				miw="25em"
+				p="xl"
+				marks={[{ value: 1, label: <Text>1</Text> }].concat(
+					Array.from({
+						length: maxLevel / 20,
+					}).map((_, index) => {
+						return {
+							value: (index + 1) * 20,
+							label: <Text>{(index + 1) * 20}</Text>,
 						}
-					}}
-				>
-					<ActionIcon variant="transparent" disabled={level === 100}>
-						<ArrowForwardIosOutlined fontSize="large" htmlColor="black" />
-					</ActionIcon>
-				</Flex>
-			</Flex>
+					})
+				)}
+				styles={{
+					markLabel: {
+						color: 'black',
+						fontSize: theme.fontSizes.xl,
+						textAlign: 'center',
+					},
+				}}
+			/>
 		</Stack>
 	)
 }
